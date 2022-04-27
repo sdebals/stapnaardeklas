d666536ed687f459277cd70efd7ccf16cab75654
Sophie Debals <sdebals@gmail.com>
2022-04-27 11:58:37 +0200

    add the pages

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+         <Head>
+            <title>Stap naar de klas | about </title>
+        </Head>
+            <h1>About</h1>
+            <p>Consequat excepteur aliquip dolor officia ullamco culpa ipsum anim excepteur mollit eiusmod. Incididunt ullamco aliqua minim excepteur consectetur dolor ut aliqua aute velit consequat. Nulla do pariatur nisi ut ad ea reprehenderit elit aute. Ea amet qui veniam non. Sunt nisi fugiat non adipisicing aute. Est id qui Lorem officia velit deserunt mollit minim cillum anim commodo.</p>
+            <p>Consequat minim est enim labore dolore veniam elit aliquip. Deserunt proident mollit sunt veniam aliquip consectetur nisi mollit consequat. Non ad ad labore voluptate aliquip anim nostrud labore veniam enim. Ipsum excepteur consequat laborum do. Elit laborum aliquip ullamco ea deserunt voluptate proident culpa ut officia non quis irure.</p>
+            <p>Laborum consectetur culpa ullamco veniam magna pariatur dolor excepteur dolor in. Nisi minim minim adipisicing duis velit cillum ea aliqua amet ex irure. Amet exercitation esse commodo minim proident dolore enim exercitation reprehenderit nostrud ea fugiat voluptate. Dolor eiusmod enim qui ullamco aliquip deserunt ad commodo pariatur consectetur proident. </p>
+        </>
+     );
+}
+ 
+export default About;
